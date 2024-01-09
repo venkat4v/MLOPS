f3d2866b67b993fd6affa2cd6644b21797eb62a3
venkat4v <venkatprasad4v@gmail.com>
2024-01-09 17:54:03 +0530

    folder structure updated

--- template.py
+++ template.py
@@ -18,6 +18,7 @@ list_of_files =[
     "src/logger/logger.py",
     "src/exception/exception.py",
     "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
     "init_setup.sh",
     "requirements.txt",
     "requirements_dev.txt",
