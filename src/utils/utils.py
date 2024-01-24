29e268231c8cf08dd22a2c1f4fd25aaec63631c6
venkat4v <venkatprasad4v@gmail.com>
2024-01-25 01:41:18 +0530

    code pipeline updated

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,7 +3,7 @@ import sys
 import pickle
 import numpy as np
 import pandas as pd
-from src.logger.logging import logging
+from src.logger.logger import logging
 from src.exception.exception import customexception
 
 from sklearn.metrics import r2_score, mean_absolute_error,mean_squared_error
