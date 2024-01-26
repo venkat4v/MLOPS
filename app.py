1101ed61dd7812e6ff82add242fa6463ddc7ed96
venkat4v <venkatprasad4v@gmail.com>
2024-01-26 17:15:20 +0530

    final changes

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def predict_datapoint():
         return render_template("result.html",final_result=result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port = 6000)
+    app.run(host="0.0.0.0",port = 9000)
